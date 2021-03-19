e705edaec1416f09258a84f8936147e49b4bd488
Jia Liu <jialiu@berkeley.edu>
2021-03-19 11:25:53 -0700

    update ngal_tomo, sigma_kappa

---
+++ sbatch.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH -C haswell
+#SBATCH -t 4:00:00
+#SBATCH -J pzNG
+#SBATCH -o pzNG.o%j
+#SBATCH -e pzNG.e%j
+#SBATCH --qos=regular
+#SBATCH -A m1727
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jialiu@berkeley.edu
+
+#export OMP_PROC_BIND=true
+#export OMP_PLACES=threads
+#export OMP_NUM_THREADS=2
+
+srun python /global/u1/j/jialiu/NGphotoz/map_stats.py
