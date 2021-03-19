2140ff0b20deac1bc5486d45147a7931d3361b8b
Jia Liu <jialiu@berkeley.edu>
2021-03-19 11:25:16 -0700

    map_stats.py test pass; ready for mass production

--- map_stats.py
+++ map_stats.py
@@ -7,10 +7,12 @@ from astropy.io import fits
 from astropy import units as u
 import os
 from lenstools import ConvergenceMap
+from emcee.utils import MPIPool 
 
 root = '/global/u1/j/jialiu/NGphotoz/'
 dir_storage = root+'NGphotoz_scratch/'
 dir_cosmos = dir_storage+'Cosmo_maps/'
+dir_cov = dir_storage+'Cov_maps/'
 
 ## constants
 map_side_deg = 10*u.degree
@@ -18,18 +20,22 @@ map_pix = 7745
 pixel_angular_side = map_side_deg / map_pix
 
 ### parameters
-sigma_e=0.26
-Nbin=20
+sigma_e = 0.26
+sigma_kappa = sqrt(2)*sigma_e
+Nbin=10 ## using 10 bins just to be safe
 theta_g_arr = [1,5,10]
 l_edges = logspace(log10(40),log10(4000),Nbin+1)
 sigma_bin_edges = linspace(-5,5,Nbin+1)
+neff = 37.0 ## chang+2013 
+ngal = neff 
+## chang+2013, n=46, raw number for fiducial case
 
 ### derived parameters
 N_theta_g = len(theta_g_arr)
-ngal_tomo = loadtxt(root+'ngal_tomo.txt')
+ngal_tomo = loadtxt(root+'ngal_tomo.txt') * ngal ## use neff instead of ngal
 sigma_kappa_arr = loadtxt(root+'sigma_kappa.txt')
 kappa_bin_edges = kron(sigma_kappa_arr,sigma_bin_edges).reshape(3,5,-1)
-sigma_pix_arr =[ (sigma_e / (pixel_angular_side * sqrt(ingal / u.arcmin**2))).decompose().value
+sigma_pix_arr =[ (sigma_kappa / (pixel_angular_side * sqrt(ingal / u.arcmin**2))).decompose().value
                 for ingal in ngal_tomo]
 
 
@@ -38,6 +44,9 @@ cosmos = [ '%02d_%s'%(i, j) for i in range(25) for j in ['a','f']]
 cosmos += ['fid_a', 'fid_f']
 cosmo_dir = '/global/cscratch1/sd/jialiu/desc-sprint-raytracing/Cosmo_maps/'
 cosmo_fn_gen = lambda cosmo, tomo, cone: cosmo_dir+cosmo+'/kappa_LSST-SRD_tomo%i_cone%i.fits'%(tomo, cone)
+
+### cov runs from 74 to 199
+cov_fn_gen = lambda tomo, cone: '/global/cscratch1/sd/jialiu/desc-sprint-raytracing/Cov_maps/kappa_LSST-SRD_tomo%i_LOS%i.fits'%(tomo, cone)
 
 def map_stats (cosmo_tomo_cone):
     '''for fits file fn, generate ps, peaks, minima, pdf, MFs
@@ -51,10 +60,15 @@ def map_stats (cosmo_tomo_cone):
     ### add noise
     ### generate random see, such that it is the same for all cosmology
     ### but different for tomo and cone
-    if cosmo[-1]=='a':
-        iseed=int(cone*100+tomo)
-    else: ##'f' starts with a different seed from the a cosmology
-        iseed=int(1000+cone*100+tomo)
+    if cone=='cov':
+        iseed=int(100000+cone*100+tomo)
+        out_dir = dir_cov
+    else:
+        if cosmo[-1]=='a':
+            iseed=int(cone*100+tomo)
+        else cosmo[-1]=='f': ##'f' starts with a different seed from the a cosmology
+            iseed=int(1000+cone*100+tomo)
+        out_dir = dir_cosmos
     seed(iseed)
     noise_map = np.random.normal(loc=0.0, scale=sigma_pix_arr[tomo-1], size=(map_pix, map_pix))
     kappa_map = ConvergenceMap(data=imap+noise_map, angle=map_side_deg)
@@ -67,7 +81,7 @@ def map_stats (cosmo_tomo_cone):
 
     s=0
     for theta_g in theta_g_arr:        
-        out_fn = dir_cosmos+cosmo+'_tomo%i_cone%i_s%i.npy'%(tomo, cone, theta_g)
+        out_fn = out_dir+cosmo+'_tomo%i_cone%i_s%i.npy'%(tomo, cone, theta_g)
         if os.path.isfile(out_fn): ## skip the calculatoin if the file is already there
             print (out_fn,'exist; skip computation.\n')
             continue
@@ -100,5 +114,20 @@ cosmo_tomo_cone_arr = [[cosmo, tomo, cone]
                        for tomo in range(1,6)
                        for cone in range(1,6)]
 
+cov_tomo_con_arr = [['cov', tomo, cone] 
+                       for tomo in range(1,6)
+                       for cone in range(74,200)]
 ## test on one map
-map_stats (cosmo_tomo_cone_arr[1])
+# map_stats (cosmo_tomo_cone_arr[1])
+
+#/global/cscratch1/sd/jialiu/desc-sprint-raytracing/Cov_maps/kappa_LSST-SRD_tomo4_LOS74.fits
+
+
+pool=MPIPool()
+if not pool.is_master():
+    pool.wait()
+    sys.exit(0)
+
+out=pool.map(map_stats, cosmo_tomo_cone_arr+cov_tomo_con_arr)
+pool.close()
+print ('DONE-DONE-DONE')
