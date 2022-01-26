7c193a0f7f6d985df0c8020decd3efbabfe7eac4
Jia Liu <jialiu@berkeley.edu>
2022-01-26 14:44:18 -0800

    fix bug: file path

--- map_stats.py
+++ map_stats.py
@@ -9,19 +9,43 @@ import os
 from emcee.utils import MPIPool # sys.modules["mpi4py"] = None
 from lenstools import ConvergenceMap
 
+######################
+###### file structure 
+######################
 root = '/global/u1/j/jialiu/NGphotoz/'
-dir_storage = root+'NGphotoz_scratch/'
 dir_SLICS = '/global/cfs/cdirs/lsst/shared/external/SLICS/desc-sprint-raytracing/'
-dir_cosmos = dir_SLICS+'Cosmo_maps/'
-dir_cov = dir_SLICS+'Cov_maps/'
-dir_bias = dir_SLICS+'Bias_maps/'
 
-## constants
+dir_storage = root+'NGphotoz_scratch/'
+dir_cosmos = dir_storage+'Cosmo_maps/'
+dir_cov = dir_storage+'Cov_maps/'
+dir_bias = dir_storage+'Bias_maps/'
+
+### tomo runs from 1-5, cone run from 1-5, cosmo run from 0-24, 25 is the fiducial model
+cosmos = [ '%02d_%s'%(i, j) for i in range(25) for j in ['a','f']]
+cosmos += ['fid_a', 'fid_f']
+cosmo_dir = dir_SLICS+'Cosmo_maps/'
+cosmo_fn_gen = lambda cosmo, tomo, cone: cosmo_dir+cosmo+'/kappa_LSST-SRD_tomo%i_cone%i.fits'%(tomo, cone)
+
+### cov runs from 74 to 199
+cov_fn_gen = lambda tomo, cone: dir_SLICS+'Cov_maps/kappa_LSST-SRD_tomo%i_LOS%i.fits'%(tomo, cone)
+### e.g. Cov_maps/kappa_LSST-SRD_tomo4_LOS74.fits
+
+### bias map gen
+bias_fn_gen = lambda tomo, ipz, cone: dir_SLICS+'biased_maps/kappa_LSST-SRD_tomo%i_%s_LOS_cone%i.fits'%(tomo, ipz, cone)
+### e.g. biased_maps/kappa_LSST-SRD_tomo5_pz_zbias0.0015_simgaz0.04_outlier0.15.txt_LOS_cone1.fits
+
+######################
+## simulation setup
+######################
+
 map_side_deg = 10*u.degree
 map_pix = 7745
 pixel_angular_side = map_side_deg / map_pix
 
-### parameters
+######################
+### fixed parameters
+######################
+
 sigma_e = 0.26
 sigma_kappa = sqrt(2)*sigma_e
 Nbin=10 ## using 10 bins just to be safe
@@ -32,29 +56,22 @@ neff = 37.0 ## chang+2013
 ngal = neff 
 ## chang+2013, n=46, raw number for fiducial case
 
+######################
 ### derived parameters
+######################
+### some of the .txt files are computed in the test notebook
+
 N_theta_g = len(theta_g_arr)
-####### the below line may have bug: to check (1/24/2022), ended up removing the *ngal part, but unsure why it was there before..
+## the below line may have bug: to check (1/24/2022), ended up removing the *ngal part, but unsure why it was there before..
 ngal_tomo = loadtxt(root+'ngal_tomo.txt') #* ngal ## use neff instead of ngal
 sigma_kappa_arr = loadtxt(root+'sigma_kappa.txt')
 kappa_bin_edges = kron(sigma_kappa_arr,sigma_bin_edges).reshape(3,5,-1)
 sigma_pix_arr =[ (sigma_kappa / (pixel_angular_side * sqrt(ingal / u.arcmin**2))).decompose().value
                 for ingal in ngal_tomo]
 
-### tomo runs from 1-5, cone run from 1-5, cosmo run from 0-24, 25 is the fiducial model
-cosmos = [ '%02d_%s'%(i, j) for i in range(25) for j in ['a','f']]
-cosmos += ['fid_a', 'fid_f']
-cosmo_dir = '/global/cscratch1/sd/jialiu/desc-sprint-raytracing/Cosmo_maps/'
-cosmo_fn_gen = lambda cosmo, tomo, cone: cosmo_dir+cosmo+'/kappa_LSST-SRD_tomo%i_cone%i.fits'%(tomo, cone)
-
-### cov runs from 74 to 199
-cov_fn_gen = lambda tomo, cone: '/global/cscratch1/sd/jialiu/desc-sprint-raytracing/Cov_maps/kappa_LSST-SRD_tomo%i_LOS%i.fits'%(tomo, cone)
-### /global/cscratch1/sd/jialiu/desc-sprint-raytracing/Cov_maps/kappa_LSST-SRD_tomo4_LOS74.fits
-
-### bias map gen
-bias_fn_gen = lambda tomo, ipz, cone: '/global/cscratch1/sd/jialiu/desc-sprint-raytracing/biased_maps/kappa_LSST-SRD_tomo%i_%s_LOS_cone%i.fits'%(tomo, ipz, cone)
-### /global/cscratch1/sd/jialiu/desc-sprint-raytracing/biased_maps/kappa_LSST-SRD_tomo5_pz_zbias0.0015_simgaz0.04_outlier0.15.txt_LOS_cone1.fits
-
+######################
+### functions 
+######################
 def map_stats (cosmo_tomo_cone):
     '''for fits file fn, generate ps, peaks, minima, pdf, MFs
     fn: input file name, including full path
@@ -66,9 +83,12 @@ def map_stats (cosmo_tomo_cone):
         ipz=''
     else:
         cosmo, tomo, cone, ipz = cosmo_tomo_cone
-
+    
+    ##################################
     ### generate random see, such that it is the same for all cosmology
     ### but different for tomo and cone
+    ##################################
+    
     if cosmo=='cov':
         iseed=int(10000+cone*10+tomo)
         out_dir = dir_cov
@@ -76,33 +96,42 @@ def map_stats (cosmo_tomo_cone):
         
     elif cosmo=='bias':
         iseed=int(20000+cone*10+tomo)#20000 
-        fn = bias_fn_gen (tomo, ipz, cone)
         out_dir = dir_bias
+        fn = bias_fn_gen (tomo, ipz, cone)
         
     else: ## all cosmologies
         if cosmo[-1]=='a':
             iseed=int(cone*10+tomo) ## cone goes from 1 to 25, so 10 to 250
-        else:# cosmo[-1]=='f': ##'f' starts with a different seed from the a cosmology
+        #else:# 
+        elif cosmo[-1]=='f': ##'f' starts with a different seed from the 'a' cosmology
             iseed=int(1000+cone*10+tomo)
         out_dir = dir_cosmos
         fn = cosmo_fn_gen(cosmo, tomo, cone)
 
     print (fn)
     
+    ##################################
+    #### check if the map and comoputed stats files are there
+    ##################################
+    
+    ############ check fits file exist
     if not os.path.isfile(fn):
-        print (fn, 'file does not exist \n')
+        print (fn, 'fits file does not exist \n')
         return 0
     
     out_fn_arr = [out_dir+cosmo+'_tomo%i_cone%s_s%i.npy'%(tomo, cone, theta_g) 
                   for theta_g in theta_g_arr]
     
+    ############# check if stats files exist; if yes, skip computation
     if np.prod(array([os.path.isfile(out_fn) for out_fn in out_fn_arr])): 
-        ### check if the product of boolean elements in the array = 1
-#         print (fn, 'stats files exist; skip computation.\n')
+        ### check if the product of boolean elements in the array = 1 (meaning for all smoothing scales)
+        ### print (fn, 'stats files exist; skip computation.\n')
         return 0 ### all files already exist, no need to process
     
-    ########## above specifies the file name, sead, output directory, below computes the stats
+    ##################################
     ########## map operations
+    ##################################
+    
     imap = fits.open(fn)[0].data ## open the file
     
     ### add noise
@@ -139,28 +168,35 @@ def map_stats (cosmo_tomo_cone):
         out[8] = mfs[1]
         out[9] = mfs[2]
         out[10] = mfs[3]        
-        save(out_fn, out)   
+        save(out_fn, out)  ### save the file 
         s+=1
     #return out
 
+##########################################
+#### specify the map file name arrays
+##########################################
 cosmo_tomo_cone_arr = [[cosmo, tomo, cone] 
                        for cosmo in cosmos 
                        for tomo in range(1,6)
                        for cone in range(1,6)]
-## test on one map
-# map_stats (cosmo_tomo_cone_arr[1])
 
 cov_tomo_cone_arr = [['cov', tomo, cone] 
                        for tomo in range(1,6)
                        for cone in range(74,1100)]#200
 ### LOS 140 and 135 are missing, 198 tomo 3, 4 are missing
-## The LOS number runs from 74 to 1100, with some missing LOS. You will have 954 in total. 
+## Joachim: The LOS number runs from 74 to 1100, with some missing LOS. You will have 954 in total. 
 
 pz_lists = genfromtxt('pz_list.txt', dtype='str')
 bias_tomo_cone_arr = [['bias', tomo, cone, ipz] 
                        for tomo in range(1,6)
                        for cone in range(1,11)
                        for ipz in pz_lists]
+
+############################
+## test on one map
+############################
+# map_stats (cosmo_tomo_cone_arr[1])
+
 
 ########################################
 ################## mass production block
